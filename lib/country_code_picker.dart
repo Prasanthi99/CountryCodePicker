629ede8cfc08c9d19990524d03528388a3cd68af
Prasanthi <prasanthi.y@technovert.com>
2020-05-28 15:38:43 +0530

    space between flag and code

--- lib/country_code_picker.dart
+++ lib/country_code_picker.dart
@@ -143,8 +143,8 @@ class CountryCodePickerState extends State<CountryCodePicker> {
                 fit: widget.alignLeft ? FlexFit.tight : FlexFit.loose,
                 child: Padding(
                   padding: widget.alignLeft
-                      ? const EdgeInsets.only(right: 16.0, left: 8.0)
-                      : const EdgeInsets.only(right: 16.0),
+                      ? const EdgeInsets.only(right: 8.0, left: 8.0)
+                      : const EdgeInsets.only(right: 8.0),
                   child: Image.asset(
                     selectedItem.flagUri,
                     package: 'country_code_picker',
